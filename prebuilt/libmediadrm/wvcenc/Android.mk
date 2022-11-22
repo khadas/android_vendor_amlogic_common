d8468926d74e248a0b72bdd6e87efda7f4bdf7b5
yongqiang.tang <yongqiang.tang@amlogic.com>
2022-11-22 11:09:37 +0800

    widevine: CB2 Unplugged HDMI playback fail retry can't recover [1/1]
    PD#OTT-34945
    
    Problem:
    Unplugged HDMI cable, playback fail retry can't recover
    
    Solution:
    HDCP Status change lead error but don't release secure resource.
    
    Verify:
    S905X4
    
    Build Tag:
    Android_R_DRM_PATCHBUILD-7519
    
    Commits:
    vendor/amlogic/common/widevine:
    version: 16.3-r57-g2b74413(build:7519)
    2b74413 widevine: CB2 Unplugged HDMI cable, playback fail retry
    can't recover [1/1]
    
    vendor/amlogic/common/tdk:
    f5fc8ed TDK: update BL32 and library [1/1]
    
    vendor/amlogic/common/tdk_v3/ta_export:
    58ea3f7 scripts: add tool to update pcpk [1/1]
    
    Git Hash-Object:
    45bd631637022504f6746af2e55bacdefa2813b1
    /arm64/liboemcrypto.so
    6ed31d82b05900360bddb75a94df048a20b8afde
    /liboemcrypto.so
    fc2219038a4aa10952a55dd93595741ec1bd113d
    /ta/v2/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    35d29add8a073a0676af8cc533ded7beda0321b1
    /ta/v2/signed/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    8132929a389052cd8fd1b7d1164f631d71b2e926
    /ta/v3/dev/A311D2/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    cc055d494e764157911b5b32e92cf9582c574312
    /ta/v3/dev/A311D2J/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    de8838c3577259dbb18f3aa4495f322cc4a6acb0
    /ta/v3/dev/POP1/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    be5e4593fd7a8292c510e038699dc9484abc6c0e
    /ta/v3/dev/S805C3L/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    bb6380bdafd0a090344d20a725feccb38ebb71ea
    /ta/v3/dev/S805X2/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    e30e91ed29ee5ffb6bc8480d1ee4c6879d0dc80f
    /ta/v3/dev/S805X2G/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    03cb822eecf8cb63a99417f7cf844a360307e9ef
    /ta/v3/dev/S905C2/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    57c7037817b2920b66b3faa3f4e42f4ad26f365b
    /ta/v3/dev/S905C2ENG/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    048e1967412150c98a2014bd744aced08c2582cf
    /ta/v3/dev/S905C2L/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    752c71a3dc6ea653d2be2d8e159dc58d00fe4d2c
    /ta/v3/dev/S905C3/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    819de678ef1158bb58a443ae7c9db64b43b72b1e
    /ta/v3/dev/S905C3ENG/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    3d9cd254fc81db2707ec6dc0175dca7cace26e81
    /ta/v3/dev/S905C3NMA/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    6d8cdc175367a1142b70497459850261aa126d48
    /ta/v3/dev/S905W2/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    a10fbec796c8fe655bfd7a058e68bdabe94f3511
    /ta/v3/dev/S905X4/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    0838161f02db1c40d7270494f26777089107cb41
    /ta/v3/dev/S905Y4/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    3bf8ba0f6bc44da1d83efb48beff7bd21e4e3bb0
    /ta/v3/dev/T963D4/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    d40fc8373ecb35f1b562be596baa7ab5eccc60db
    /ta/v3/dev/T965D4/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    0f306708e75545db083ae44be61a5d85d4eb90f7
    /ta/v3/dev/T982/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    01494448da0052b671af26d7635f43f66f6bae8f
    /ta/v3/dev/V918D/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    b8ba3b97e31b19d2db006a31324a87b2fae2194d
    /ta/v3/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    8a04245167b8a3ee87da1f28caba5949c2c009ae
    /ta/v3/signed/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta
    
    Change-Id: I8424c165230cc2c4383f3bc1ca9c61e685e6ba5a
    Signed-off-by: yongqiang.tang <yongqiang.tang@amlogic.com>

--- prebuilt/libmediadrm/wvcenc/Android.mk
+++ prebuilt/libmediadrm/wvcenc/Android.mk
@@ -24,21 +24,15 @@ TA_UUID := e043cde0-61d0-11e5-9c26-0002a5d5c51b
 TA_SUFFIX := .ta
 
 ifeq ($(PLATFORM_TDK_VERSION), 38)
-PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk_v3
-ifeq ($(BOARD_AML_SOC_TYPE),)
-LOCAL_TA := ta/v3/signed/$(TA_UUID)$(TA_SUFFIX)
+	PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk_v3
+	ifeq ($(BOARD_AML_SOC_TYPE),)
+		LOCAL_TA := ta/v3/signed/$(TA_UUID)$(TA_SUFFIX)
+	else
+		LOCAL_TA := ta/v3/dev/$(BOARD_AML_SOC_TYPE)/$(TA_UUID)$(TA_SUFFIX)
+	endif
 else
-LOCAL_TA := ta/v3/dev/$(BOARD_AML_SOC_TYPE)/$(TA_UUID)$(TA_SUFFIX)
-endif
-else
-PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk
-LOCAL_TA := ta/v2/$(TA_UUID)$(TA_SUFFIX)
-endif
-
-ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
-ENCRYPT := 1
-else
-ENCRYPT := 0
+	PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk
+	LOCAL_TA := ta/v2/signed/$(TA_UUID)$(TA_SUFFIX)
 endif
 
 LOCAL_SRC_FILES := $(LOCAL_TA)
@@ -48,14 +42,6 @@ LOCAL_MODULE_SUFFIX := $(TA_SUFFIX)
 LOCAL_STRIP_MODULE := false
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
-ifneq ($(PLATFORM_TDK_VERSION), 38)
-ifeq ($(TARGET_ENABLE_TA_SIGN), true)
-LOCAL_POST_INSTALL_CMD = $(PLATFORM_TDK_PATH)/ta_export/scripts/sign_ta_auto.py \
-		--in=$(shell pwd)/$(LOCAL_MODULE_PATH)/$(TA_UUID)$(LOCAL_MODULE_SUFFIX) \
-		--keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH) \
-		--encrypt=$(ENCRYPT)
-endif
-endif
 include $(BUILD_PREBUILT)
 
 endif # LOCAL_OEMCRYPTO_LEVEL == 1
