4680d4555ec4eb336981de8a1083841584c3af67
chang.qi <chang.qi@amlogic.com>
2023-09-01 09:51:23 +0800

    gtv : gtv lancher start  slow [1/1]
    PD#SWPL-129712
    
    Problem:
    gtv lancher start  slow
    
    Solution:
    set cpu set
    
    Verify:
    local
    
    Change-Id: I64210a129ddf191b5947b61c8f6cc008f92ed06c
    Signed-off-by: chang.qi <chang.qi@amlogic.com>

--- frameworks/core/res/src/com/droidlogic/DroidLogicCpuService.java
+++ frameworks/core/res/src/com/droidlogic/DroidLogicCpuService.java
@@ -9,7 +9,12 @@ import com.droidlogic.app.SystemControlManager;
 
 public class DroidLogicCpuService extends Service {
     private static final String TAG = "DroidLogicCpuService";
-    private SystemControlManager mSystemControlManager = null;
+    private SystemControlManager mSCM = null;
+    private String mTop;
+    private String mForeground;
+    private String mBackground;
+    private String mSystem;
+    private String mRestricted;
 
     private void setCpusetsDefault() {
         new Thread() {
@@ -17,11 +22,11 @@ public class DroidLogicCpuService extends Service {
             public void run() {
                 try {
                     Thread.sleep(1000*60*3);
-                    mSystemControlManager.writeSysFs("/dev/cpuset/top-app/cpus", "0-3");
-                    mSystemControlManager.writeSysFs("/dev/cpuset/foreground/cpus", "0-3");
-                    mSystemControlManager.writeSysFs("/dev/cpuset/background/cpus", "0-3");
-                    mSystemControlManager.writeSysFs("/dev/cpuset/system-background/cpus", "0-3");
-                    mSystemControlManager.writeSysFs("/dev/cpuset/restricted/cpus", "0-3");
+                    mSCM.writeSysFs("/dev/cpuset/top-app/cpus", mTop);
+                    mSCM.writeSysFs("/dev/cpuset/foreground/cpus", mForeground);
+                    mSCM.writeSysFs("/dev/cpuset/background/cpus", mBackground);
+                    mSCM.writeSysFs("/dev/cpuset/system-background/cpus", mSystem);
+                    mSCM.writeSysFs("/dev/cpuset/restricted/cpus", mRestricted);
                     Log.d(TAG, "end setCpusetsDefault: 0-3");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -29,6 +34,19 @@ public class DroidLogicCpuService extends Service {
             }
         }.start();
 
+    }
+
+    private void initCpusets() {
+        mTop = mSCM.readSysFsOri("/dev/cpuset/top-app/cpus");
+        Log.d(TAG, "top cpus is  " + mTop);
+        mForeground = mSCM.readSysFsOri("/dev/cpuset/foreground/cpus");
+        Log.d(TAG, "foreground cpus is  " + mForeground);
+        mBackground = mSCM.readSysFsOri("/dev/cpuset/background/cpus");
+        Log.d(TAG, "background cpus is  " + mBackground);
+        mSystem = mSCM.readSysFsOri("/dev/cpuset/system-background/cpus");
+        Log.d(TAG, "system background cpus is  " + mSystem);
+        mRestricted = mSCM.readSysFsOri("/dev/cpuset/restricted/cpus");
+        Log.d(TAG, "restricted cpus is  " + mRestricted);
     }
 
     @Override
@@ -40,8 +58,9 @@ public class DroidLogicCpuService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "DroidLogicCpuService is oncreate");
-        mSystemControlManager = SystemControlManager.getInstance();
+        mSCM = SystemControlManager.getInstance();
         Log.d(TAG, "start setCpusetsDefault: 0-3");
+        initCpusets();
         setCpusetsDefault();
     }
 
