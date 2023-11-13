1b3845baaf2da1d25711e83fd1d3883e3df4b952
chong.huang <chong.huang@amlogic.com>
2023-11-13 15:02:34 +0800

    SystemControl: remove dummy when change DV [1/2]
    PD#SWPL-161132
    
    Problem:
    remove dummy when change DV.
    
    Solution:
    remove dummy when change DV.
    
    Verify:
    t7c
    
    Change-Id: Ic485100ddb502e98a2b3a3807a094d5fcbcbd3b4
    Signed-off-by: chong.huang <chong.huang@amlogic.com>

--- frameworks/services/systemcontrol/DisplayMode.cpp
+++ frameworks/services/systemcontrol/DisplayMode.cpp
@@ -986,6 +986,7 @@ void DisplayMode::applyDisplaySetting(hdmi_output_info_t* output_info) {
     // 5. check dolby vision
     int  dv_type    = DOLBY_VISION_SET_DISABLE;
     bool dv_change  = false;
+    bool dvmode_change = false;
 
     dv_type   = output_info->dv_type;
     dv_change = checkDolbyVisionStatusChanged(dv_type);
@@ -996,15 +997,18 @@ void DisplayMode::applyDisplaySetting(hdmi_output_info_t* output_info) {
             && (strstr(hdr_policy, HDR_POLICY_SINK))) {
             pSysWrite->writeSysfs(DISPLAY_HDMI_AVMUTE_SYSFS, "1");
         }
-        //5.2 set dummy_l mode when dv change at UI switch
-        if ((OUTPUT_MODE_STATE_SWITCH == output_info->reason) && dv_change) {
-            setDisplayMode("dummy_l");
-        }
-        //5.3 enable or disable dolby vision core
-        if (DOLBY_VISION_SET_DISABLE != dv_type) {
-            enableDolbyVision(dv_type);
-        } else {
-            disableDolbyVision(dv_type);
+        //5.2 Set dv_type by scene
+        //5.2.1 set dvmode_change to true when dv change at UI switch
+        //set avmute-close phy>-set hdr/dv policy>set mode-clear avmute ---enable hdcp
+        if (OUTPUT_MODE_STATE_SWITCH == output_info->reason) {
+            dvmode_change = true;
+        }  else {
+            //5.2.2 In other scenarios, set DV directly
+            if (DOLBY_VISION_SET_DISABLE != dv_type) {//enable or disable dolby vision core
+                enableDolbyVision(dv_type);
+            } else {
+                disableDolbyVision(dv_type);
+            }
         }
 
         SYS_LOGI("isDolbyVisionEnable [%d] dolby vision type:%d", isDolbyVisionEnable(), getDolbyVisionType());
@@ -1030,7 +1034,7 @@ void DisplayMode::applyDisplaySetting(hdmi_output_info_t* output_info) {
     //7. check any change
     bool isNeedChange = false;
 
-    if (modeChange || attr_change || frac_rate_policy_change || hdr_policy_change || hdr_priority_change) {
+    if (modeChange || attr_change || frac_rate_policy_change || hdr_policy_change || hdr_priority_change || dvmode_change) {
         isNeedChange = true;
     } else {
         SYS_LOGI("nothing need to be changed\n");
@@ -1107,6 +1111,16 @@ void DisplayMode::applyDisplaySetting(hdmi_output_info_t* output_info) {
         //apply hdr priority to driver sysfs
         if (hdr_priority_change) {
             DisplayModeMgr::getInstance().setDisplayAttribute(DISPLAY_HDR_PRIORITY, HDR_PRIORITY_TYPE[hdr_priority], ConnectorType::CONN_TYPE_HDMI);
+        }
+
+        //apply enable or disable dolby vision core
+        if (dvmode_change) {
+            if (DOLBY_VISION_SET_DISABLE != dv_type) {
+                enableDolbyVision(dv_type);
+            } else {
+                disableDolbyVision(dv_type);
+            }
+            SYS_LOGI("isDolbyVisionEnable [%d] dolby vision type:%d", isDolbyVisionEnable(), getDolbyVisionType());
         }
 
         //set hdmi mode
