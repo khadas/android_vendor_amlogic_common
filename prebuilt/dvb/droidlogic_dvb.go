514d8d7935b2d942a22313e03b5f3fdf5b1f59a9
kieth liu <kieth.liu@amlogic.com>
2023-11-23 20:31:31 +0800

    commom/dvb:  fix build error after removing TARGET_BUILD_LIVETV [1/1]
    PD#SWPL-146894
    
    Problem:
    cbs build error after removing TARGET_BUILD_LIVETV
    
    Solution:
    modify dvbSrcPath and icuSrcPath
    
    Verify:
    ohm
    
    Signed-off-by: kieth liu <kieth.liu@amlogic.com>
    Signed-off-by: wenjie ren <wenjie.ren@amlogic.com>
    Change-Id: I2285d25e7bac150ae2b8f86276457183352ea8ed

--- prebuilt/dvb/droidlogic_dvb.go
+++ prebuilt/dvb/droidlogic_dvb.go
@@ -1,7 +1,7 @@
 package droidlogic_dvb
 
 import (
-    // "fmt"
+    //"fmt"
     "android/soong/android"
     "android/soong/cc"
     "github.com/google/blueprint/proptools"
@@ -19,14 +19,15 @@ func dvb_go_DefaultsFactory() (android.Module) {
         }
         p := &props{}
 
-       dvbSrcPath := "vendor/amlogic/common/external/dvb"
-       icuSrcPath := "vendor/amlogic/common/external/icu"
-        if android.ExistentPathForSource(ctx, dvbSrcPath).Valid() == true {
+       dvbSrcPath := "vendor/amlogic/reference/external/dvb"
+       icuSrcPath := "vendor/amlogic/reference/external/icu"
+       
+       if android.ExistentPathForSource(ctx, dvbSrcPath).Valid() == true {
             p.Enabled = proptools.BoolPtr(false)
-            // fmt.Printf("dvb:%s exist, use source to build\n", dvbSrcPath)
+            //fmt.Printf("dvb:%s exist, use source to build\n", dvbSrcPath)
         } else if android.ExistentPathForSource(ctx, icuSrcPath).Valid() == true {
             p.Enabled = proptools.BoolPtr(false)
-            // fmt.Printf("dvb:%s not exist, use prebuilt to build\n", icuSrcPath)
+            //fmt.Printf("dvb:%s not exist, use prebuilt to build\n", icuSrcPath)
         }
         ctx.AppendProperties(p)
     })
