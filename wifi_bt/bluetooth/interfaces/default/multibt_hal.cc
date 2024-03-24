f0fb204347f845dc51038cd821103ed1e56803ed
Zhu Lv <zhu.lv@amlogic.com>
2024-03-24 11:52:57 +0800

    bt: solve multibt coverity warning [1/1]
    PD#SWPL-160259
    
    Problem:
    multibt coverity warning
    
    Solution:
    solve multibt coverity warning
    
    Verify:
    ohm
    
    Change-Id: I1d41be6c8d14e1921bb5e0f9b3e3d48002e45392
    Signed-off-by: Zhu Lv <zhu.lv@amlogic.com>

--- wifi_bt/bluetooth/interfaces/default/multibt_hal.cc
+++ wifi_bt/bluetooth/interfaces/default/multibt_hal.cc
@@ -82,16 +82,16 @@ typedef struct {
 
 typedef struct {
     dev_id mod_id;
-    char dev_name[20];
-    char vnd_lib_name[64];
-    char mod_name[20];
+    char dev_name[PROP_VALUE_MAX];
+    char vnd_lib_name[PROP_VALUE_MAX];
+    char mod_name[PROP_VALUE_MAX];
     char power_type;
 } dev_info;
 
 typedef struct {
     unsigned int vid;
-    char dev_name[20];
-    char vnd_lib_name[64];
+    char dev_name[PROP_VALUE_MAX];
+    char vnd_lib_name[PROP_VALUE_MAX];
     char power_type;
 } dev_info_uart;
 
@@ -318,11 +318,11 @@ static void set_bt_prop(const char *dev_name, const char *mod_name, const char *
         return;
     }
 
-    memcpy(bt_prop_val.dev_name, dev_name, sizeof(bt_prop_val.dev_name));
+    memcpy(bt_prop_val.dev_name, dev_name, (sizeof(bt_prop_val.dev_name) - 1));
     property_set(PROP_BT_NAME, bt_prop_val.dev_name);
-    memcpy(bt_prop_val.mod_name, mod_name, sizeof(bt_prop_val.mod_name));
+    memcpy(bt_prop_val.mod_name, mod_name, (sizeof(bt_prop_val.mod_name) - 1));
     property_set(PROP_BT_MODULE, bt_prop_val.mod_name);
-    memcpy(bt_prop_val.vnd_lib_name, vnd_lib_name, sizeof(bt_prop_val.vnd_lib_name));
+    memcpy(bt_prop_val.vnd_lib_name, vnd_lib_name, (sizeof(bt_prop_val.vnd_lib_name) - 1));
     property_set(PROP_LIBBT_VENDOR, bt_prop_val.vnd_lib_name);
 }
 
@@ -375,9 +375,10 @@ static bool matching_dev_id_uart(unsigned int vid)
         if (bt_dev_uart[cnt].vid == vid) {
             PR_INFO("matched vid:%4x, dev_name:%s, cnt:%u, set property",bt_dev_uart[cnt].vid,
                 bt_dev_uart[cnt].dev_name, cnt);
-            memcpy(bt_prop_val.dev_name, bt_dev_uart[cnt].dev_name, sizeof(bt_prop_val.dev_name));
+            memcpy(bt_prop_val.dev_name, bt_dev_uart[cnt].dev_name, (sizeof(bt_prop_val.dev_name) - 1));
             property_set(PROP_BT_NAME, bt_prop_val.dev_name);
-            memcpy(bt_prop_val.vnd_lib_name, bt_dev_uart[cnt].vnd_lib_name, sizeof(bt_prop_val.vnd_lib_name));
+            memcpy(bt_prop_val.vnd_lib_name, bt_dev_uart[cnt].vnd_lib_name,
+                (sizeof(bt_prop_val.vnd_lib_name) - 1));
             property_set(PROP_LIBBT_VENDOR, bt_prop_val.vnd_lib_name);
             ret = true;
             break;
@@ -815,8 +816,8 @@ static bool distinguish_bt_module_pci(void)
     }
 
     while ((next = readdir(dir)) != NULL) {
-        char line[256]= {'\0'};
-        char uevent_file[256] = {'\0'};
+        char line[256] = {'\0'};
+        char uevent_file[512] = {'\0'};
 
         /* Read pci uevent file, uevent's data like below:
          * DRIVER=w2_comm
@@ -851,6 +852,11 @@ static bool distinguish_bt_module_pci(void)
                 goto exit;
             }
         }
+
+        if (fp) {
+            fclose(fp);
+            fp = NULL;
+        }
     }
 
 exit:
@@ -880,8 +886,8 @@ static bool distinguish_bt_module_sdio(void)
     }
 
     while ((next = readdir(dir)) != NULL) {
-        char line[256]= {'\0'};
-        char uevent_file[256] = {'\0'};
+        char line[256] = {'\0'};
+        char uevent_file[512] = {'\0'};
 
         /* Read sdio uevent file, uevent's data like below:
          * DRIVER=aml_sdio
@@ -918,6 +924,11 @@ static bool distinguish_bt_module_sdio(void)
                 goto exit;
             }
         }
+
+        if (fp) {
+            fclose(fp);
+            fp = NULL;
+        }
     }
 
 exit:
@@ -947,8 +958,8 @@ static bool distinguish_bt_module_usb(void)
     }
 
     while ((next = readdir(dir)) != NULL) {
-        char line[256]= {'\0'};
-        char uevent_file[256] = {'\0'};
+        char line[256] = {'\0'};
+        char uevent_file[512] = {'\0'};
 
         /* Read usb uevent file, uevent's data like below:
          * MAJOR=189
@@ -986,6 +997,11 @@ static bool distinguish_bt_module_usb(void)
             if (ret) {
                 goto exit;
             }
+        }
+
+        if (fp) {
+            fclose(fp);
+            fp = NULL;
         }
     }
 
@@ -1772,7 +1788,7 @@ static bool distinguish_bt_module(void)
                 goto exit;
             }
 
-            while (retry_cnt_usb <= 20) {  // usb distinguish retry maximum delay 400ms
+            while (retry_cnt_usb < 20) {  // usb distinguish retry maximum delay 400ms
                 usleep(20000);
                 retry_cnt_usb ++;
                 PR_INFO("usb distinguish retry_cnt_usb:%u", retry_cnt_usb);
