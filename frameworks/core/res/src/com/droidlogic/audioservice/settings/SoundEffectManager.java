8fd6ca08e8f91f579075893b5b07885c09e43061
hongchao.yin <hongchao.yin@amlogic.com>
2024-04-10 12:17:12 +0800

    audio: start track after creating sound effects [1/1]
    PD#SWPL-159587
    
    Problem:
    AC off/on, small window sound becomes louder.
    
    Solution:
    1. need apply audio effect after creating sound effects
    
    Verify:
    U + T5W
    
    Change-Id: I93cf3e0ee63eb6b23622e853e8426390bb6193cf
    Signed-off-by: hongchao.yin <hongchao.yin@amlogic.com>

--- frameworks/core/res/src/com/droidlogic/audioservice/settings/SoundEffectManager.java
+++ frameworks/core/res/src/com/droidlogic/audioservice/settings/SoundEffectManager.java
@@ -314,6 +314,7 @@ public class SoundEffectManager {
 
         creatDpeAudioEffect();
         mEffectInit = true;
+        applyAudioEffectByPlayEmptyTrack();
     }
 
     public void cleanupAudioEffects() {
@@ -1046,11 +1047,14 @@ public class SoundEffectManager {
 
     private void applyAudioEffectByPlayEmptyTrack() {
         int bufsize = AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-        if (bufsize <= 0)
+        if (bufsize <= 0) {
+            Log.w(TAG, "applyAudioEffectByPlayEmptyTrack buffersize:" + bufsize + " invalid.");
             return;
+        }
         byte data[] = new byte[bufsize];
         AudioTrack trackplayer = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_STEREO,
                 AudioFormat.ENCODING_PCM_16BIT, bufsize, AudioTrack.MODE_STREAM);
+        Log.i(TAG, "applyAudioEffectByPlayEmptyTrack create empty AudioTrack to applay AudioEffect");
         trackplayer.play();
         for (int i = 0; i <= 5; i++) {
             trackplayer.write(data, 0, data.length);
@@ -1195,7 +1199,6 @@ public class SoundEffectManager {
            Log.e(TAG, "init DAP2.4 ok");
            initDap_2_4();
        }
-       applyAudioEffectByPlayEmptyTrack();
    }
 
 
@@ -1789,8 +1792,6 @@ public class SoundEffectManager {
                 setDpeParam(id, getDpeParam(id));
             }
         }
-
-        applyAudioEffectByPlayEmptyTrack();
     }
 
 
@@ -3057,6 +3058,7 @@ public class SoundEffectManager {
             creatDapAudioEffect();
             initDapAudioEffect();
         }
+        applyAudioEffectByPlayEmptyTrack();
     }
     public void setAudioEffectOnByIndex (int id, boolean dbSwitch) {
         if (DroidLogicUtils.getAudioDebugEnable()) Log.d(TAG, "setAudioEffectOnByIndex id:" + id + ", dbSwitch:" + dbSwitch);
@@ -3123,6 +3125,7 @@ public class SoundEffectManager {
                 Log.e(TAG, "setAudioEffectOnByIndex id:" + id + " is invalid!");
                 break;
         }
+        applyAudioEffectByPlayEmptyTrack();
     }
 
     public void setAudioEffectOn (int id, boolean dbSwitch) {
